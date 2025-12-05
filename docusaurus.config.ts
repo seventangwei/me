3b11470037ae40268797d50977db953bc8f10003
sevenkztang <sevenkztang@tencent.com>
2025-12-05 16:00:33 +0800

    fix: baseurl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
   url: 'https://senventangwei.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/me',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
